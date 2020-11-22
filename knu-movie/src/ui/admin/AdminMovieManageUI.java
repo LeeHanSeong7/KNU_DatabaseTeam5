5ce77aeffc212abf32a61c092bd1a95f1f8abdd7
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2020-11-22 19:51:37 +0900

    ui update
    ui update

--- knu-movie/src/ui/admin/AdminMovieManageUI.java
+++ knu-movie/src/ui/admin/AdminMovieManageUI.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.sql.Connection;
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import injected.DIContainer.Services;
@@ -45,7 +46,7 @@ public class AdminMovieManageUI {
 					for(i=0;i<movieDTOList.size();i++){
 		                MovieDTO item = movieDTOList.get(i);
 		                System.out.println("<"+i+">");
-		                System.out.println("[id:"+item.getTitleId()+"]title:" + item.getTitle());
+		                System.out.println("[id:"+item.getTitleId()+"] title:" + item.getTitle());
 		                System.out.println("type: " + item.getType() + ", region: " + item.getRegion());
 		                System.out.println("genre: " + item.getGenreList());
 		                System.out.println("casting: " + item.getActorList());
@@ -103,20 +104,61 @@ public class AdminMovieManageUI {
 		}
 	}
 	Boolean modify(MovieDTO data) {
-		MovieDTO movieData = MovieUITool.makeMovieDTO(
-				new MovieDTO(null, null, null, null, null, null, null, null, null, new ArrayList<String>(), null, new ArrayList<String>()));
-		if (movieData == null) {
-			System.out.println("modify failed");
-			return false;
+		MovieDTO movieData = new MovieDTO(null, null, null, null, null, null, null, null, null, new ArrayList<String>(), null, new ArrayList<String>());
+		String str;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("---information---");
+		System.out.print("title 	: ");
+		str = scan.nextLine();
+		if (str.length() != 0) movieData.setTitle(str);
+		System.out.print("region 	: ");
+		str = scan.nextLine();
+		if (str.length() != 0) movieData.setRegion(str);
+		System.out.print("runtime 	: ");
+		str = scan.nextLine();
+		try {
+			Integer.parseInt(str);
+			movieData.setRuntime(str);
 		}
-		else {
-			Result result = movieService.updateMovie(data, movieData);
-			if (result == Result.success)
-				System.out.println("modify conplete");
-			else {
-				System.out.println(result.getError().getDescription());
-				return false;
+		catch(Exception e) {
+			System.out.println("wrong format, please modify later");
+		}
+		System.out.print("startyear(yyyy-mm-dd) : ");
+		str = scan.nextLine();
+		try {
+			Date.valueOf(str);
+			movieData.setRuntime(str);
+		}
+		catch(Exception e) {
+			System.out.println("wrong format, please modify later");
+		}
+		System.out.print("genres , 'ex) war , romance , comedy': ");
+		str = scan.nextLine();
+		if (str.length() != 0) {
+			try {
+				ArrayList<String> temp = new ArrayList<String>();
+				temp = (ArrayList<String>)Arrays.asList(str.replaceAll(" ", "").split(","));
+				movieData.setGenreList(temp);
 			}
+			catch(Exception e) {
+				movieData.setGenreList(null);
+				//System.out.println("wrong format, please modify later");
+				e.getStackTrace();
+			}
+		}
+		else
+			movieData.setGenreList(null);
+		System.out.print("type 		: ");
+		str = scan.nextLine();
+		if (str.length() != 0) movieData.setType(str);
+		System.out.println("-----------------");
+		
+		Result result = movieService.updateMovie(data, movieData);
+		if (result == Result.success)
+			System.out.println("modify conplete");
+		else {
+			System.out.println(result.getError().getDescription());
+			return false;
 		}
 		return true;
 	}
