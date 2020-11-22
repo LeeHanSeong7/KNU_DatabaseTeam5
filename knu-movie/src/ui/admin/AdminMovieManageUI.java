9c792662dd82d2693b6fd17f1349774236d1fc53
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2020-11-22 18:30:51 +0900

    movie manage ui update
    ui update

--- knu-movie/src/ui/admin/AdminMovieManageUI.java
+++ knu-movie/src/ui/admin/AdminMovieManageUI.java
@@ -1,21 +1,26 @@
 package ui.admin;
 
+import java.lang.reflect.Array;
 import java.sql.Connection;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import injected.DIContainer.Services;
 import pd.interfaces.MovieService;
 import pd.model.AccountDTO;
+import pd.model.MovieDTO;
 import pd.model.MovieSearchConditionDTO;
+import pd.utils.Result;
+import ui.MovieUITool;
 import ui.SearchUITool;
 
 public class AdminMovieManageUI {
 	private Connection conn;
-	private MovieService MovieService;
+	private MovieService movieService;
 	
 	public AdminMovieManageUI(MovieService services) {
-		MovieService = services;
+		movieService = services;
 	}
 	public void setConnection(Connection conn) {
 		this.conn = conn;
@@ -25,7 +30,7 @@ public class AdminMovieManageUI {
 		while(true) {
 			String str;
 			System.out.println("-movie manage menu-");
-			System.out.println("0.help   1.search   2.upload   3.modify   4.delete   5.back");
+			System.out.println("0.help   1.search   2.upload   3.back");
 			str = scan.nextLine();
 			str = str.replaceAll(" ", "");
 			if (str.equals("0")) {
@@ -33,57 +38,106 @@ public class AdminMovieManageUI {
 			else if(str.equals("1")){
 				MovieSearchConditionDTO condition = SearchUITool.makeMovieSearchCondition();
 				System.out.println("--result--");
+				Result result = movieService.searchMoiveByCondition(condition);
+				if (result == Result.success) {
+					ArrayList<MovieDTO> movieDTOList = (ArrayList<MovieDTO>)result.getValue();
+					int i;
+					for(i=0;i<movieDTOList.size();i++){
+		                MovieDTO item = movieDTOList.get(i);
+		                System.out.println("<"+i+">");
+		                System.out.println("[id:"+item.getTitleId()+"]title:" + item.getTitle());
+		                System.out.println("type: " + item.getType() + ", region: " + item.getRegion());
+		                System.out.println("genre: " + item.getGenreList());
+		                System.out.println("casting: " + item.getActorList());
+		                System.out.println("runtime: " + item.getRuntime()+"min "+ ", startYear: " + item.getStartYear());
+		                System.out.println("avgRating: " + String.valueOf(item.getAvg()) + ", numVotes: " + item.getNumVotes());
+		            }
+					int size = i;
+					System.out.print("if you want to manage movie, give index :");
+					try {
+						str = scan.nextLine();
+						int index = Integer.parseInt(str);
+						if (size > index && index >= 0) {
+							System.out.print("1.modify   2.delete :");
+							str = scan.nextLine();
+							if (str.equals("1")) {
+								modify(movieDTOList.get(index));
+							}
+							else if (str.equals("2")) {
+								delete(movieDTOList.get(index));
+							}
+							else {
+								System.out.println("wrong format.");
+							}
+						}
+						else
+							System.out.println("wrong format.");
+					}
+					catch(Exception e) {
+						System.out.println("wrong format.");
+					}
+			    }
+			    else {
+			    	System.out.println(result.getError().getDescription());
+			    }
 			}
 			else if(str.equals("2")){
-				String title_id;
-			    String type;
-			    double runtime;
-			    Date start_year;
-		   
-				System.out.println("---insert information---");
-				System.out.print("title_id : ");
-				title_id = scan.nextLine();
-				System.out.print("type : ");
-				type = scan.nextLine();
-				System.out.print("runtime :");
-				try {
-					runtime = Double.parseDouble(scan.nextLine());
-				}
-				catch(Exception e) {
-					System.out.println("type error");
+				MovieDTO movieData = MovieUITool.makeMovieDTO(
+						new MovieDTO(null, null, null, null, null, 0, null, 0, 0, new ArrayList<String>(), null, new ArrayList<String>()));
+				if (movieData == null) {
+					System.out.println("upload failed");
 					continue;
 				}
-				System.out.print("start_year (yyyy-mm-dd) :");
-				try {
-					start_year = Date.valueOf(scan.nextLine());
-				}
-				catch(Exception e) {
-					System.out.println("type error");
-					continue;
-				}
-				//Result result = services.authenticationService.signUp("id", "password", new accountDTO(null, null, null, ...));
-			    if (true == true) {
-					System.out.println("upload complete!");
-			    }
-			    else {}
-			}
-			else if(str.equals("3")){
-				System.out.print("give movie id for modify :");
-				while(true) {
-					System.out.print("give modify operation, 'column:value'. type 'end' if you want to end : ");
-					str = scan.nextLine();
-					if (str.equals("end")) break;
-					else
-						System.out.println("--result--");
+				else {
+//					Result result = movieService.movieUpload(movieData);
+//					if (result == Result.success)
+//						System.out.println("upload conplete");
+//					else {
+//						System.out.println(result.getError().getDescription());
+//						continue;
+//					}
 				}
 			}
-			else if(str.equals("4")){
-				System.out.print("give movie id for delete :");
-				str = scan.nextLine();
-				System.out.println("--result--");
-			}
-			else if(str.equals("5"))	break;
+			else if(str.equals("3"))	break;
 			else System.out.println("invalid operation");
 		}
+	}
+	Boolean modify(MovieDTO data) {
+		Scanner scan = new Scanner(System.in);
+		MovieSearchConditionDTO condition = new MovieSearchConditionDTO();
+		condition.fillWithDefault();
+		System.out.print("MovieId	: ");
+		condition.movieID = scan.nextLine();
+		System.out.print("Region	: ");
+		condition.movieName = scan.nextLine();
+		Result result = movieService.searchMoiveByCondition(condition) ;
+		if (result == Result.success) {
+			ArrayList<MovieDTO> temp = (ArrayList<MovieDTO>)result.getValue();
+			MovieDTO movieData = temp.get(0);
+			movieData = MovieUITool.makeMovieDTO(movieData);
+			
+			//result = movieService.movieDelete(condition.movieID,condition.movieName);
+			// 재업로드
+		}
+		else
+			System.out.println(result.getError().getDescription());
+		return true;
+	}
+	Boolean delete(MovieDTO data) {
+		Scanner scan = new Scanner(System.in);
+		String str;
+		System.out.print("delete this movie ? 'y' :");
+		str = scan.nextLine();
+		if (str.equals("y") || str.equals("Y")) {
+			Result result = movieService.movieDelete(data.getTitleId());
+			if (result == Result.success)
+				System.out.println("movie has deleted");
+			else 
+				System.out.println(result.getError().getDescription());
+		}
+		else {
+			return false;
+		}
+		return true;
 	}
 }
